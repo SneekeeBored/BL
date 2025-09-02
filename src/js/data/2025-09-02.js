1871280083de922ffed6f6a190f93b6d9b92ebf3
SneekeeBored <97010036+SneekeeBored@users.noreply.github.com>
2025-09-02 09:39:15 +0200

    Update 2025-09-02.js

--- src/js/data/2025-09-02.js
+++ src/js/data/2025-09-02.js
@@ -37,16 +37,627 @@ dataSet[dataSetVersion].options = [
 ];
 
 dataSet[dataSetVersion].characterData = [
-{
-  name: "Michael Kaiser",
-  img: "kaiser.png",
-  opts: {
-    team: ["ngxi","bm","m"]
+  {
+    name: "Adam",
+    img: "adam.jpg",
+    opts: {
+      team: ["wf","m"]
+    }
   },
-{
-  name: "Yoichi Isagi",
-  img: "isagi.png",
-  opts: {
-    team: ["tz","bleleven","ju20","m"]
+  {
+    name: "Agi",
+    img: "agi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Akira",
+    img: "akira.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Anri Teieri",
+    img: "anri.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Jyubei Aryu",
+    img: "aryu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Meguru Bachira",
+    img: "bachira.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Shoei Barou",
+    img: "barou.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Bello",
+    img: "bello.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Birkenstock",
+    img: "birkenstock.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Bunny Iglesias",
+    img: "bunny.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Hirotoshi Buratsuta",
+    img: "buratsuta.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Charles Chevalier",
+    img: "charles.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Hyoma Chigiri",
+    img: "chigiri.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Choki",
+    img: "choki.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Chris Prince",
+    img: "chris.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Dada Silva",
+    img: "dada.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Miroku Darai",
+    img: "darai.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Jinpachi Ego",
+    img: "ego.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Erik Gesner",
+    img: "erik.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Gen Fukaku",
+    img: "fukaku.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Gin Gagamaru",
+    img: "gagamaru.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ginji Hiori",
+    img: "ginji.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Benedict Grim",
+    img: "grim.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Shizuka Haiji",
+    img: "haiji.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Haru Hayate",
+    img: "haru.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Aiki Himizu",
+    img: "himizu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Yo Hiori",
+    img: "hiori.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Okuhito Iemon",
+    img: "iemon.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Gurimu Igarashi",
+    img: "igarashi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Yudai Imamura",
+    img: "imamura.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Yoichi Isagi",
+    img: "isagi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Issei Isagi",
+    img: "issei.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Itsuki Wakatsuki",
+    img: "itsuki.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Iyo Isagi",
+    img: "iyo.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Junko Hiori",
+    img: "junko.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Junichi Wanima",
+    img: "jwanima.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Michael Kaiser",
+    img: "kaiser.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Tabito Karasu",
+    img: "karasu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kazuma Nio",
+    img: "kazuma.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kento Cho",
+    img: "kento.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ryosuke Kira",
+    img: "kira.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Jin Kiyora",
+    img: "kiyora.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Koyuki Chigiri",
+    img: "koyuki.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Rensuke Kunigami",
+    img: "kunigami.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Wataru Kuon",
+    img: "kuon.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ranze Kurona",
+    img: "kurona.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kuso",
+    img: "kuso.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Keisuke Wanima",
+    img: "kwanima.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kyohei Shiguma",
+    img: "kyohei.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Lavinho",
+    img: "lavinho.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Julian Loki",
+    img: "loki.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Don Lorenzo",
+    img: "lorenzo.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Leonardo Luna",
+    img: "luna.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Mick Moon",
+    img: "mick.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Seishiro Nagi",
+    img: "nagi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Nijiro Nanase",
+    img: "nanase.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Asahi Naruhaya",
+    img: "naruhaya.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Neneko Chigiri",
+    img: "neneko.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Teppei Neru",
+    img: "neru.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Alexis Ness",
+    img: "ness.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ikki Niko",
+    img: "niko.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Hajime Nishioka",
+    img: "nishioka.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Noel Noa",
+    img: "noa.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Oboabona",
+    img: "oboabona.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Hibiki Okawa",
+    img: "okawa.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Oliver Aiku",
+    img: "oliver.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Onazi",
+    img: "onazi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Eita Otoya",
+    img: "otoya.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Pablo Cavasoz",
+    img: "pablo.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Jingo Raichi",
+    img: "raichi.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ray Dark",
+    img: "ray.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Reiji Hiiragi",
+    img: "reiji.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Reo Mikage",
+    img: "reo.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Rin Itoshi",
+    img: "rin.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Sae Itoshi",
+    img: "sae.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kairu Saramadara",
+    img: "saramadara.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Shuto Sendo",
+    img: "sendo.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Ryusei Shidou",
+    img: "shidou.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Marc Snuffy",
+    img: "snuffy.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Taiga Tsunzaki",
+    img: "taiga.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Shingen Tanaka",
+    img: "tanaka.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Teru Kitsunezato",
+    img: "teru.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Tetsu Sokura",
+    img: "tetsu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Aoshi Tokimitsu",
+    img: "tokimitsu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Yu Bachira",
+    img: "yu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Kenyu Yukimiya",
+    img: "yukimiya.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Yukio Ishikari",
+    img: "yukio.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Haruhiko Yuzu",
+    img: "yuzu.jpg",
+    opts: {
+      team: []
+    }
+  },
+  {
+    name: "Zantetsu Tsurugi",
+    img: "zantetsu.jpg",
+    opts: {
+      team: []
+    }
   }
-}
+];
